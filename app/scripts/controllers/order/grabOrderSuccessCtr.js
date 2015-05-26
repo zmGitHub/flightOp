ff2ac0e9b8be263adf0dae92af4db1e4b6eff7f9
mclaren <18065355@qq.com>
2015-05-26 09:53:56 +0800

    修改原因：

--- app/scripts/controllers/order/grabOrderSuccessCtr.js
+++ app/scripts/controllers/order/grabOrderSuccessCtr.js
@@ -5,15 +5,15 @@
  */
 angular.module('flightOpApp').controller('grabOrderSuccessCtr', ['$scope', 'orderSuccessList', 'Order',function($scope, orderSuccessList ,Order){
   var vm = $scope.vm = {};
-  vm.orderList = orderSuccessList;
+  vm.orderSuccessList = orderSuccessList;
   console.log(orderSuccessList);
   vm.showOverlay = false; /*刷新*/
   $scope.reload = function(){
     vm.showOverlay = true;
     /*刷新订单列表*/
-    Order.getAllOrder().then(function(res){
+    Order.grabSuccessOrder().then(function(res){
       if(res){
-        vm.orderList = res;
+        vm.orderSuccessList = res;
         vm.showOverlay = false;
       }
     });
